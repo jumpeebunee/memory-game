c8d728a7592e1684c2cdbd2e731a07c601b2b5b3
Ben <jumpeebunee@gmail.com>
2022-07-16 13:44:43 +0300

    feat: add more functionality

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 function startGame() {
     const container = document.querySelector('.container');
-    const button = document.querySelector('.btn');
     const content = document.createElement('div');
     content.classList.add('content');
-
-    button.addEventListener('click', startButton);
 
     const stack = [];
     let prewCard = [];
 
     let choices = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 
-    function startButton() {
-        button.classList.add('btn-start');
-        button.addEventListener('transitionend', () => starGame());
-    };
+    gameStarted();
 
-    function starGame() {
-        button.remove();
+    function gameStarted() {
         container.append(content);
         for (let i = 1; i <= 16; i++) {
             const card = document.createElement('div');
             card.dataset.value = getRandomNumber();
             card.classList.add('block');
+            getEvent(card);
             content.append(card);
         };
+    };
+
+    function getUserCard(e) {
+        const current = e.target;
+        const currentValue = current.dataset.value;
+        current.classList.add('is-open');
+        current.textContent = currentValue;
+
+        current.removeEventListener('click', getUserCard);
+        prewCard.push(current);
+
+        if (stack.length === 0) {
+            stack.push(currentValue);
+        } else if (stack[0] === currentValue) {
+            prewCard = [];
+            stack.pop();
+        } else {
+            setTimeout(() => {
+                prewCard.forEach((item) => {
+                    item.classList.remove('is-open');
+                    item.textContent = '';
+                    getEvent(item);
+                    prewCard = [];
+                    stack.pop();
+                });
+            }, 1000)
+        };
+    };
+
+    function getEvent(eventList) {
+        eventList.addEventListener('click', getUserCard);
     };
 
     function getRandomNumber() {
@@ -37,50 +62,3 @@ function startGame() {
 
 document.addEventListener('DOMContentLoaded', startGame);
 
-/*
-    container.append(content);
-    const stack = [];
-    let arrD = []
-
-    let arr = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
-
-    for (let i = 1; i <= 16; i++) {
-        const card = document.createElement('div');
-        card.classList.add('block');
-        card.dataset.value = getNumber();
-        card.addEventListener('click', getCard);
-        content.append(card);
-    };
-
-    function getNumber() {
-        let random = arr[Math.floor(Math.random() * (arr.length - 0)) + 0];
-        arr.splice(arr.indexOf(random), 1);
-        return random;
-    };
-
-    function getCard(e) {
-
-        let current = e.target;
-        current.classList.add('is-open');
-        current.textContent = e.target.dataset.value;
-
-        arrD.push(e.target)
-
-        if (stack.length === 0) { 
-            stack.push(e.target.dataset.value);
-        } else if (stack[0] === e.target.dataset.value) {
-            arrD = [];
-            stack.pop();
-        } else {
-            setTimeout(() => {
-                arrD.forEach((item) => {
-                    item.classList.remove('is-open');
-                    item.textContent = '';
-                    arrD = [];
-                    stack.pop();
-                });
-            },2000)
-        };
-
-    };
-*/
